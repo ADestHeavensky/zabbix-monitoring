81cb351a39d58bcb63e25836f86c875f17a40281
ADest_Heavensky <144311392+ADestHeavensky@users.noreply.github.com>
2025-12-09 05:08:26 +0300

    Create proxy-network.sh

---
+++ proxy-network.sh
@@ -0,0 +1,27 @@
+#!/bin/bash
+
+echo "Производится настройка второго сервера"
+
+echo "Настройка узлов сети..."
+sudo cp hosts /etc/hosts
+
+echo "Настройка имени узла..."
+sudo hostnamectl set-hostname server2
+
+echo "Настройка интерфейсов"
+sudo cp interfaces2 /etc/network/interfaces
+sudo systemctl restart networking
+
+echo "Настройка SSH..."
+sudo apt-get update
+sudo apt-get install openssh-server -y
+sudo sed -i 's/^#\?Port.*/Port 22/' /etc/ssh/sshd_config
+sudo sed -i 's/^#\?PermitRootLogin.*/PermitRootLogin no/' /etc/ssh/sshd_config
+sudo sed -i 's/^#\?PasswordAuthentication.*/PasswordAuthentication no/' /etc/ssh/sshd_config
+sudo systemctl restart sshd
+
+echo "Копирование данных SSH..."
+sudo mkdir -p /root/.ssh
+sudo cp authorized_keys /root/.ssh/
+
+/bin/bash
