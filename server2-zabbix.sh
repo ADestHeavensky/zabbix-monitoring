071529fe57367435ec17051f92c43d12c7f9a441
ADest_Heavensky <144311392+ADestHeavensky@users.noreply.github.com>
2025-12-09 09:36:15 +0300

    Create server2-zabbix.sh

---
+++ server2-zabbix.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+
+echo "Установка Zabbix Server для server2"
+
+sudo wget https://repo.zabbix.com/zabbix/7.4/release/debian/pool/main/z/zabbix-release/zabbix-release_latest_7.4+debian12_all.deb
+sudo dpkg -i zabbix-release_latest_7.4+debian12_all.deb
+sudo apt-get update
+sudo apt-get install -y zabbix-server-mysql zabbix-frontend-php zabbix-apache-conf zabbix-sql-scripts zabbix-agent
+sudo cp zabbix_server2.conf /etc/zabbix/zabbix_server.conf
+
+echo "Установка и настройка MariaDB для server2"
+
+sudo apt-get install rsync mariadb-server galera-4 -y
+sudo systemctl stop mariadb
+sudo cp galera2.cnf /etc/mysql/conf.d/galera.cnf
